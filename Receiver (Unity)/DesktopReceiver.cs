e0caab6d0e7c84979fdd22b25727efa91806f1e9
idealexe <ideal.exe@live.jp>
2017-08-03 14:46:25 +0900

    Update

---
+++ Receiver (Unity)/DesktopReceiver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DesktopReceiver : MonoBehaviour {
+    public static int port = 3389; // 一般的なリモートデスクトップのポートに合わせた
+    private UdpClient udpClient;
+    private IPEndPoint endPoint;
+    private const int MAX_BUFFER_SIZE = 1024 * 1024 * 8; // 8MB
+    private byte[] imgBuffer = new byte[MAX_BUFFER_SIZE]; // UDPで受信した画像データを格納する
+
+    // 初期化
+    void Start()
+    {
+        // UDPクライアントの作成
+        endPoint = new IPEndPoint(IPAddress.Any, port);
+        udpClient = new UdpClient(endPoint);
+    }
+
+    // 毎フレーム行う処理
+    void Update () {
+
+        while (udpClient.Available > 0)
+        {
+            imgBuffer = udpClient.Receive(ref endPoint);
+            Texture2D tex = new Texture2D(1920, 1200);
+            tex.LoadImage(imgBuffer, true);
+            GetComponent<Renderer>().material.mainTexture = tex;
+        }
+    }
+
+}
